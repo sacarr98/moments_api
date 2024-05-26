389d36ac3c4c6f54d53ba14492a9a18cd503d265
Sophie Carr <sacarr98@gmail.com>
2024-05-26 10:16:44 +0000

    add follower app, views and serializer, and refactor post and profile views

---
+++ followers/serializers.py
@@ -0,0 +1,22 @@
+from django.db import IntegrityError
+from rest_framework import serializers
+from followers.models import Follower
+
+
+class FollowerSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Like model
+    The create method handles the unique constraint on 'owner' and 'post'
+    """
+    owner = serializers.ReadOnlyField(source='owner.username')
+    followed_name = serializers.ReadOnlyField(source='followed.username')
+
+    class Meta:
+        model = Like
+        fields = ['id', 'created_at', 'owner', 'followed', 'followed_name']
+    
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({'detail': 'possible duplicate'})
