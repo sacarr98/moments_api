c3762f57b677ceb985d0469211536b503c2e6bfb
Sophie Carr <sacarr98@gmail.com>
2024-05-29 13:00:24 +0000

    fix typo in settings.py

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -63,7 +63,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['8000-sacarr98-momentsapi-1ls7yw2hzhq.ws-eu114.gitpod.io', os.envrion.get('ALLOWED_HOST'), 'localhost',]
+ALLOWED_HOSTS = ['8000-sacarr98-momentsapi-1ls7yw2hzhq.ws-eu114.gitpod.io', os.environ.get('ALLOWED_HOST'), 'localhost',]
 
 
 # Application definition
@@ -112,7 +112,7 @@ if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN')
     ]
-    
+
 if 'CLIENT_ORIGIN_DEV' in os.environ:
     extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
